f88f60e03ed9105d55d61da12e4857286f6f78aa
Akanksha Verma <akanksha3454verma@gmail.com>
2022-04-23 06:32:44 +0530

    entrepreneur model added

---
+++ models/Entrepreneur.js
@@ -0,0 +1,37 @@
+//User Schema
+
+const mongoose = require("mongoose");
+
+const EntrepreneurSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+      },
+    
+     
+    
+      website: {
+        type: String
+      },
+    
+      photo: {
+        type: String
+      },
+    
+      bio: {
+        type: String
+      },
+    
+      address: {
+        type: String
+      },
+    
+    //   backedProjects: [{
+    //     type: _mongoose2.default.Schema.Types.ObjectId,
+    //     ref: 'Project'
+    //   }],
+});
+
+const User = mongoose.model("Entrepreneur", UserSchema);
+
+module.exports = Entrepreneur;
